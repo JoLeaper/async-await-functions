908961bdc5827d8bb9522372f5204ab6d9d8c1a9
JoLeaper <joleap123@gmail.com>
2020-06-22 17:02:36 -0700

    seed 5 movie test passing

--- _tests_/seed.test.js
+++ _tests_/seed.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect.js');
 const Movie = require('../models/Movie');
 const Review = require('../models/Review');
+const seedData = require('../lib/seed');
 
 describe('product routes', () => {
   beforeAll(async() => {
@@ -62,27 +63,8 @@ describe('product routes', () => {
   });
 
   it('checks if 5 movies are created', async() => {
-    const movie1 = await Movie.create({
-      title: 'The Boom',
-      description: 'Big Boom',
-      studio: 'SuperLarge'
-    });
-
-    const review1 = await Review.create({
-      movie: movie1._id,
-      authorName: 'Guy Guy',
-      comment: 'It was alright',
-    });
-
-    expect(review1.toJSON()).toEqual({
-      _id: review1._id,
-      movie: movie1._id,
-      authorName: 'Guy Guy',
-      comment: 'It was alright',
-      reviewDate: expect.any(Date),
-      latestUpdate: expect.any(Date),
-      __v: 0
-    });
+    await seedData();
+    expect(await Movie.find()).toHaveLength(5);
   });
 
   // it('checks if 100 reviews are created', async() => {
